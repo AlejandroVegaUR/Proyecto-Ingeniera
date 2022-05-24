eeff1a277d88b8d1545f0f2a833f9b8ff55447b7
AlejandroVegaUR <94658310+AlejandroVegaUR@users.noreply.github.com>
2022-05-24 08:18:04 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,126 @@
+
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+from Connection_p import Connection
+import plotly.graph_objects as go
+import proyecto  as sql
+
+app = dash.Dash(__name__)
+
+con = Connection()
+con.openConnection()
+# queries
+query_1 = pd.read_sql_query(sql.vuelo_tickets(), con.connection)
+query_2 = pd.read_sql_query(sql.vuelo_hora(), con.connection)
+query_3 = pd.read_sql_query(sql.num_aviones(), con.connection)
+query_4 = pd.read_sql_query(sql.nacionalidad(), con.connection)
+query_5 = pd.read_sql_query(sql.destino(), con.connection)
+query_6 = pd.read_sql_query(sql.modelos(), con.connection)
+query_7 = pd.read_sql_query(sql.nacionalidad_asc(), con.connection)
+con.closeConnection()
+#Dataframes
+aeropuertos = pd.DataFrame(query_1, columns=["id_vuelo","num_tickets"])
+vuelos_hora= pd.DataFrame(query_2, columns=["hora_llegada", "total"])
+num_aviones= pd.DataFrame(query_3, columns=["aerolinea", "num_aviones"])
+nacionalidad= pd.DataFrame(query_4, columns=["nacionalidad", "total"])
+nacionalidad_asc= pd.DataFrame(query_7, columns=["nacionalidad", "total"])
+destino= pd.DataFrame(query_5, columns=["ciudad", "total"])
+modelos= pd.DataFrame(query_6, columns=["nombre", "total"])
+print(nacionalidad)
+
+#Crear las gráficas
+fig=px.bar(aeropuertos,x="id_vuelo",y="num_tickets",title="Número de tiquetes por vuelo")
+fig_2=px.line(vuelos_hora,x="hora_llegada",y="total",title="Número de vuelos que llegan por hora")
+fig_3=px.bar(num_aviones,x="aerolinea",y="num_aviones",title="Número de aviones por aerolinea")
+fig_5=px.bar(nacionalidad, x='nacionalidad',y="total",title="Flujo de turistas por nacionalidad")
+#fig_4 = px.scatter(nacionalidad, x="nacionalidad", y="total",size="total",title="Nacionalidad con mayor flujo de turistas")
+fig_4 = px.scatter_geo(destino, locations="ciudad", size="total",projection="natural earth",title="Mapa mundi")
+fig_6=px.pie(modelos, values='total', names='nombre',title="Modelos de aviones")
+
+fig_7=px.scatter_geo(destino, locations="ciudad", size="total",projection="natural earth",title="Mapa mundi")
+
+
+#actualizar los estilos de las gráficas
+fig.update_traces(marker_color='rgb(199, 0, 57)', marker_line_color='rgb(144, 12, 63)',
+                  marker_line_width=1.5, opacity=0.6)
+fig_3.update_traces(marker_color='rgb(199, 0, 57)', marker_line_color='rgb(144, 12, 63)',
+                  marker_line_width=1.5, opacity=0.6)
+
+fig.update_layout(paper_bgcolor = "#D7EDFA ", font = {'color': "black", 'family': "Times New Roman"})
+fig_2.update_layout(paper_bgcolor = "#BDD5E4 ", font = {'color': "black", 'family': "Times New Roman"})
+fig_3.update_layout(paper_bgcolor = "#D7EDFA", font = {'color': "black", 'family': "Times New Roman"})
+fig_4.update_layout(paper_bgcolor = "#D7EDFA", font = {'color': "black", 'family': "Times New Roman"})
+fig_5.update_layout(paper_bgcolor = "#D7EDFA", font = {'color': "black", 'family': "Times New Roman"})
+fig_6.update_layout(paper_bgcolor = "#BDD5E4", font = {'color': "black", 'family': "Times New Roman"})
+
+app.layout = html.Div(children=[ 
+            html.Div('Base de datos aérea',style={ 'family': "Times New Roman", 'fontSize': 50,'text-align':"center"}
+            ),
+              
+            dcc.Graph(
+            id='mapa',
+            figure=fig_7,
+            ),
+            
+            dcc.Graph(
+            id='modelos',
+            figure=fig_6,
+            ),
+            
+            
+            dcc.Graph(
+            id='tiquetes',
+            figure=fig,
+            ),
+              
+            dcc.Graph(
+            id='llegadas',
+            figure=fig_2,
+            ),
+              
+            dcc.Graph(
+            id='aviones',
+            figure=fig_3,
+            ),dcc.Dropdown( id = 'menu',
+            options = [
+                {'label':'Top 10 nacionalidades ascendentemente', 'value':'asc' },
+                {'label': 'Top 10 nacionalidades descendentemente', 'value':'desc'},
+                
+                ],
+            style={'backgroundColor':"#e4e4e6;"}
+            ),
+            html.Button('Ver', id='b_ver', n_clicks=0, style={'backgroundColor':'#D7EDFA'}),
+            dcc.Graph(
+            id='nacionalidad',
+            ),
+                                ],
+        style={'backgroundColor':'#BDD5E4'})
+@app.callback( 
+        dash.dependencies.Output("nacionalidad","figure"),
+        dash.dependencies.Input("menu","value"),
+        dash.dependencies.Input('b_ver', 'n_clicks'),
+       
+    )
+def update_graph(value,n_clicks):
+    df=nacionalidad
+    if n_clicks!=0:
+        if value is not None:
+            if value=="asc":
+                df=nacionalidad_asc
+                
+            print(df)
+      
+    fig_5=px.bar(df, x='nacionalidad',y="total",title="Flujo de turistas por nacionalidad")
+    fig_5.update_layout(paper_bgcolor = "#BDD5E4", font = {'color': "black", 'family': "Times New Roman"})
+    fig_5.update_traces(marker_color='rgb(199, 0, 57)', marker_line_color='rgb(144, 12, 63)',
+                  marker_line_width=1.5, opacity=0.6)   
+    return fig_5
+    
+if __name__ == "__main__":
+    app.run_server(debug=True)
+    
+
+
